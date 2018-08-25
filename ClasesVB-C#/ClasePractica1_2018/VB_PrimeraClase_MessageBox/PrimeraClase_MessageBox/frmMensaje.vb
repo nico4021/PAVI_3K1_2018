e7c13403695fa160875ef85e65cb832e0f7bdd44
Soledad <sromero@proveedor.personal.com.ar>
2018-08-25 20:21:36 -0300

    clases 01 02 03 04 - material base de datos - tutoriales

---
+++ ClasesVB-C#/ClasePractica1_2018/VB_PrimeraClase_MessageBox/PrimeraClase_MessageBox/frmMensaje.vb
@@ -0,0 +1,13 @@
+﻿Public Class frmMensaje
+
+    Private Sub btnMensaje_Click(sender As Object, e As EventArgs) Handles btnMensaje.Click
+        Dim mensaje As String = txtMensaje.Text
+
+
+        'MessageBox.Show("Esto es un ejemplo!", "Título")
+        'MessageBox.Show(mensaje, "Título")
+        MsgBox(mensaje, MsgBoxStyle.Information, "Título")
+    End Sub
+
+
+End Class
---
+++ ClasesVB-C#/ClasePractica4_2018/C#_TPS_BugTracker/TPS_BugTracker/frmBugs.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TPS_BugTracker
+{
+    public partial class frmBugs : Form
+    {
+        public frmBugs()
+        {
+            InitializeComponent();
+        }
+    
+        private void llenarCombo(ComboBox cbo, Object source, string display, String value)
+        {
+            cbo.DataSource = source;
+            cbo.DisplayMember = display;
+            cbo.ValueMember = value;
+            cbo.SelectedIndex = -1;
+        }
+
+        private void frmBugs_Load(object sender, EventArgs e)
+        {
+            BDHelper helper = new BDHelper();
+            //LLenar combos y limpiar grid
+            llenarCombo(cboEstados, helper.ConsultaSQL("Select * from Estados"), "n_estado", "id_estado");
+            llenarCombo(cboPrioridades, helper.ConsultaSQL("Select * from Prioridades"), "n_prioridad", "id_prioridad");
+            llenarCombo(cboCriticidades, helper.ConsultaSQL("Select * from Criticidades"), "n_criticidad", "id_criticidad");
+            llenarCombo(cboAsignadoA, helper.ConsultaSQL("Select * from users"), "n_usuario", "id_usuario");
+            llenarCombo(cboProductos, helper.ConsultaSQL("Select * from Productos"), "nombre", "id_producto");
+
+        }
+
+        private void btnConsultar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
